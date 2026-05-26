28961e8e4e07d96e07fd221c7de3b2cc3e5752db
Sangram Botre <sbotre461@gmail.com>
2026-05-26 20:49:36 +0530

    LF-205 Optimized application configuration and connection pooling

--- src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,12 +2,12 @@ package com.bezkoder.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringBootDataJpaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
-
 }
